9a128a2786fe753b31cf6fb4379d041b9352c593
Babou-Dev <baptiste.oger@icloud.com>
2021-02-18 23:03:14 +0100

    Initial commit

---
+++ src/be/baboudev/GameFrame.java
@@ -0,0 +1,18 @@
+package be.baboudev;
+
+import javax.swing.*;
+
+public class GameFrame extends JFrame {
+
+    GameFrame(){
+
+        this.add(new GamePanel());
+        this.setTitle("SnakeGame");
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setResizable(false);
+        this.pack();
+        this.setVisible(true);
+        this.setLocationRelativeTo(null);
+
+    }
+}
---
+++ src/be/baboudev/SnakeGame.java
@@ -0,0 +1,9 @@
+package be.baboudev;
+
+public class SnakeGame {
+
+    public static void main(String[] args) {
+        new GameFrame();
+    }
+
+}
